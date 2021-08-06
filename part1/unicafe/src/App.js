0675ff83e2bf69bab3960bb200b283f7ef1d4061
Caleb Phillips <csphillips5@gmail.com>
2021-08-05 21:46:26 -0400

    implement display of total, average, and percent positive feedback

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
 )
 
-const Display = (props) => (
-  <p>{props.text} {props.amount}</p>
+const DisplayEachFeedback = ({text, amount}) => (
+  <p>{text} {amount}</p>
+)
+
+const AllFeedback = ({total}) => (
+  <p>all {total}</p>
+)
+
+const AverageScore = ({good, bad, total}) => (
+  <p>average {(good - bad) / total || 0}</p>
+)
+
+const PositiveFeedback = ({good, total}) => (
+  <p>positive {(good / total) * 100 || 0} %</p>
 )
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = (good + neutral + bad);
 
   return (
     <div>
@@ -20,9 +33,12 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <h1>statistics</h1>
-      <Display text='good' amount={good} />
-      <Display text='neutral' amount={neutral} />
-      <Display text='bad' amount={bad} />
+      <DisplayEachFeedback text='good' amount={good} />
+      <DisplayEachFeedback text='neutral' amount={neutral} />
+      <DisplayEachFeedback text='bad' amount={bad} />
+      <AllFeedback total={total} />
+      <AverageScore good={good} bad={bad} total={total} />
+      <PositiveFeedback good={good} total={total} />
     </div>
   );
 }
