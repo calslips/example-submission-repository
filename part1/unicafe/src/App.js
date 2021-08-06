cdd4933818f19889e28bd893198ce3eed55d2333
Caleb Phillips <csphillips5@gmail.com>
2021-08-05 22:17:48 -0400

    implement Statistics component 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,27 +4,25 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
 
-const DisplayEachFeedback = ({text, amount}) => (
-  <p>{text} {amount}</p>
-)
+const Statistics = ({good, neutral, bad}) => {
+  const total = (good + neutral + bad);
 
-const AllFeedback = ({total}) => (
-  <p>all {total}</p>
-)
-
-const AverageScore = ({good, bad, total}) => (
-  <p>average {(good - bad) / total || 0}</p>
-)
-
-const PositiveFeedback = ({good, total}) => (
-  <p>positive {(good / total) * 100 || 0} %</p>
-)
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total ||0}</p>
+      <p>positive {(good / total) * 100 || 0}</p>
+    </div>
+  );
+}
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = (good + neutral + bad);
 
   return (
     <div>
@@ -33,12 +31,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <h1>statistics</h1>
-      <DisplayEachFeedback text='good' amount={good} />
-      <DisplayEachFeedback text='neutral' amount={neutral} />
-      <DisplayEachFeedback text='bad' amount={bad} />
-      <AllFeedback total={total} />
-      <AverageScore good={good} bad={bad} total={total} />
-      <PositiveFeedback good={good} total={total} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
