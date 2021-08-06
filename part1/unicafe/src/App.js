f8407578170f044e02460cee3c40382532769795
Caleb Phillips <csphillips5@gmail.com>
2021-08-05 20:53:40 -0400

    implemented functionality to collect/display user feedback 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 
-const App = () = => {
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
+const Display = (props) => (
+  <p>{props.text} {props.amount}</p>
+)
+
+const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <h1>statistics</h1>
+      <Display text='good' amount={good} />
+      <Display text='neutral' amount={neutral} />
+      <Display text='bad' amount={bad} />
     </div>
   );
 }
