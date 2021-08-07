429a3a68fd7fce1ec7d4b00430af1c9f4f2cdecc
Caleb Phillips <csphillips5@gmail.com>
2021-08-06 20:44:29 -0400

    display statistics in HTML table format

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,19 +9,24 @@ const Statistics = ({good, neutral, bad, total}) => {
     return <p>No feedback given</p>
   }
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={total} />
-      <StatisticLine text='average' value={(good - bad) / total || 0} />
-      <StatisticLine text='positive' value={(good / total) * 100 || 0} percentSign='%' />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={total} />
+        <StatisticLine text='average' value={(good - bad) / total || 0} />
+        <StatisticLine text='positive' value={(good / total) * 100 || 0} percentSign='%' />
+      </tbody>
+    </table>
   );
 }
 
 const StatisticLine = ({text, value, percentSign}) => (
-  <p>{text} {value} {percentSign}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value} {percentSign}</td>
+    </tr>
 );
 
 const App = () => {
