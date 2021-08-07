56b9d5a143c5a8fa27fc55d2bc82fa71d604b6f6
Caleb Phillips <csphillips5@gmail.com>
2021-08-06 21:40:46 -0400

    add button to click to display random anecdote

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -11,10 +15,12 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is the same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ];
   const [selected, setSelected] = useState(0);
+  const randomIndex = () => Math.floor(Math.random() * anecdotes.length);
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={() => setSelected(randomIndex)} text='next anecdote' />
     </div>
   );
 }
