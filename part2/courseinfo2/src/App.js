b943e2d637fe4b2f1f1dd0c1cf5dbe2b6326b4e4
Caleb Phillips <csphillips5@gmail.com>
2021-08-13 00:23:57 -0400

    implement Course component and app to work regardless of # of parts, 2.1

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -18,38 +18,45 @@ const Part = (props) => (
 
 const Content = ({ course }) => (
   <div>
-    <Part part={course.parts[0]} />
-    <Part part={course.parts[1]} />
-    <Part part={course.parts[2]} />
+    {course.parts.map((part, i) => (
+      <Part key={course.parts[i].id} part={course.parts[i]} />
+    ))}
   </div>
 );
 
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
