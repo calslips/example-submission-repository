04be97f11f4677205710700917c940e0df0a3351
Caleb Phillips <csphillips5@gmail.com>
2021-08-14 06:04:33 -0400

    implement functionality that enables user to add phone numbers, 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,28 +4,36 @@ const List = ({ persons }) => {
   return persons.map((person) => <Person key={person.name} person={person}/>)
 };
 
-const Person = ({ person }) => <p>{person.name}</p>
+const Person = ({ person }) => <p>{person.name} {person.number}</p>
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]);
   const [ newName, setNewName ] = useState('add a person!');
+  const [ number, setNumber ] = useState('and their number!');
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNumber(event.target.value);
+  }
+
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
-      name: newName
+      name: newName,
+      number: number
     }
     checkName(newName) ?
       sameName(newName) :
       setPersons(persons.concat(personObject));
     setNewName('');
+    setNumber('');
   }
 
   const checkName = (newName) => {
@@ -46,6 +54,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={number} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
